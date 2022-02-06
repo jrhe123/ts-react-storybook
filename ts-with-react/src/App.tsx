affaaf0685f081396069b53ac19cea9a442d39a7
jrhe1213 <jiarong1213@gmail.com>
2022-02-05 23:00:52 -0500

    setup react demo

---
+++ ts-with-react/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hello from "./components/Hello";
+
+function App() {
+  return (
+    <div className="App">
+      <Hello message="123" />
+    </div>
+  );
+}
+
+export default App;
