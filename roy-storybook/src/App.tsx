534f620ec6fa27cb8e9fb66b8fc5b0b2104f2fbd
jrhe1213 <jiarong1213@gmail.com>
2022-02-06 16:10:35 -0500

    alert element

--- roy-storybook/src/App.tsx
+++ roy-storybook/src/App.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Button, { ButtonType, ButtonSize } from "./components/Button/button";
+import Alert, { AlertType } from "./components/Alert/alert";
 
 function App() {
+  const [toggle, setToggle] = useState(true);
   return (
     <div>
+      {toggle && (
+        <Alert
+          alertType={AlertType.Danger}
+          title="this is my title"
+          message="this is my message"
+          onClick={() => {
+            setToggle(!toggle);
+          }}
+        >
+          this is my children
+        </Alert>
+      )}
       <Button className="abc">default</Button>
       <Button disabled>default disabled</Button>
       <Button btnType={ButtonType.Primay} size={ButtonSize.Large}>
---
+++ roy-storybook/src/components/Alert/alert.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import classNames from "classnames";
+
+export enum AlertType {
+  Success = "success",
+  Default = "default",
+  Danger = "danger",
+  Warning = "warning",
+}
+
+interface BaseAlertProps {
+  className?: string;
+  title?: string;
+  message?: string;
+  alertType?: AlertType;
+  children?: React.ReactNode;
+}
+
+type NativeAlertProps = BaseAlertProps &
+  React.ButtonHTMLAttributes<HTMLElement>;
+
+const Alert: React.FC<NativeAlertProps> = (props) => {
+  const { alertType, children, className, title, message, ...restProps } =
+    props;
+  const classes = classNames("alert", className, {
+    [`alert-${alertType}`]: alertType,
+  });
+  return (
+    <div className={classes}>
+      {title && <p>{title}</p>}
+      {message && <p>{message}</p>}
+      <button {...restProps}>X</button>
+      {children}
+    </div>
+  );
+};
+
+Alert.defaultProps = {
+  message: "this is alert!",
+  alertType: AlertType.Default,
+};
+
+export default Alert;
